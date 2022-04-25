6e91f30cc56812823e8429312a5fb2e5cb64f7d2
Frank McDermott <fjmcde@gmail.com>
2022-04-24 23:15:01 -0600

    Separated main file from code

---
+++ main.py
@@ -0,0 +1,23 @@
+from knightsTour import Tour
+
+## Driver code to run a knights tour
+## Just provide:
+## - The size of the board (Note: must be N x N);
+## - A starting position for the knight (Note: rows and columns begin with 0
+##   so there are only N - 1 valid spaces; whereN = NUMROWS OR NUMCOLS);
+## - Instantiate a knight object by passing the size and starting position 
+##   of the knight into the constructor
+## - Call the solve() function
+def main():
+    NUMROWS = 8
+    NUMCOLS = 8
+    STARTX = 0
+    STARTY = 0
+
+    knight = Tour(NUMROWS, NUMCOLS, STARTX, STARTY)
+    knight.solve()
+
+    return 0
+
+if __name__ == "__main__":
+    main()
